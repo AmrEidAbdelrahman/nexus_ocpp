e2a788d2f275af07d5bb0d42bd65252655048439
AmrEidAbdelrahman <amr.eid@uniparticle.com>
2025-03-08 14:56:38 +0200

    configure database connection postgres

--- ocpp_server/settings.py
+++ ocpp_server/settings.py
@@ -111,17 +111,17 @@ WSGI_APPLICATION = 'ocpp_server.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'ocpp_server',
-    #     'USER': 'ocpp_user',
-    #     'PASSWORD': 'ocpp_pass',
-    #     'HOST': 'db',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ocpp_server',
+        'USER': 'ocpp_user',
+        'PASSWORD': 'ocpp_pass',
+        'HOST': 'db',
+    }
 }
 
 
